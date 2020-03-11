4995719de7fd617824c4d149293f183d8dcbe0c0
Misael Vasconcelos <misael@diproach.com>
2020-03-11 15:35:26 -0300

    added splash and icon

---
+++ src/pages/home/home.ts
@@ -0,0 +1,24 @@
+import { Component } from '@angular/core';
+import { NavController } from 'ionic-angular';
+
+@Component({
+  selector: 'page-home',
+  templateUrl: 'home.html'
+})
+export class HomePage {
+  priceBeforeIva: any;
+  priceWithIVA: any;
+  constructor(
+    public navCtrl: NavController,
+    ) {
+
+  }
+  calculatePrice(value){
+    let dolar:number = 62.68
+    let input:number = parseFloat(value)
+    this.priceBeforeIva = (input * dolar).toFixed(2)
+    let priceNoIva = input * dolar
+    this.priceWithIVA = (priceNoIva + (30*priceNoIva)/100).toFixed(2)
+    console.log(input, value, this.priceBeforeIva, this.priceWithIVA)
+  }
+}
